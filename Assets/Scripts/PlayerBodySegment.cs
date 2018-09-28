61f535421fecbe5aaceb7160226c75006d60a03a
Kayne Ruse <kayneruse@gmail.com>
2018-09-29 09:01:58 +1000

    Basic single player snake working

---
+++ Assets/Scripts/PlayerBodySegment.cs
@@ -0,0 +1,66 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerBodySegment : MonoBehaviour {
+	//cached components
+	SpriteRenderer spriteRenderer;
+
+	public GameObject parent;
+	public float speed = 1.0f;
+
+	GameObject child;
+	GameObject head;
+
+	void Start() {
+		spriteRenderer = GetComponent<SpriteRenderer>();
+	}
+
+	void Update() {
+		HandleMovement();
+		HandleGraphics();
+	}
+
+	void HandleMovement() {
+		//move faster if further away
+		float multiplier;
+		if ((parent.transform.position - transform.position).normalized.magnitude > 1f) {
+			multiplier = 1.2f;
+		} else {
+			multiplier = 1f;
+		}
+
+		//actually move
+		transform.position += (parent.transform.position - transform.position).normalized * speed * Time.deltaTime * multiplier;
+	}
+
+	void HandleGraphics() {
+		if (child == null) {
+			spriteRenderer.color = Color.yellow;
+		}
+	}
+
+	public void Duplicate(int n, float parentSpeed, GameObject headObject) {
+		speed = parentSpeed * 0.9f;
+		if (n <= 2) {
+			return;
+		}
+
+		if (child == null) {
+			child = Instantiate(this.gameObject);
+		}
+
+		PlayerBodySegment childScript = child.GetComponent<PlayerBodySegment>();
+
+		childScript.parent = this.gameObject;
+		childScript.Duplicate(n - 1, speed, headObject);
+
+		head = headObject;
+	}
+
+	void OnCollisionEnter2D(Collision2D collision) {
+		if (collision.gameObject.GetComponent<PlayerHead>() != null) {
+			Debug.Log("eating...");
+		}
+	}
+}
---
+++ Assets/Scripts/PlayerHead.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerHead : MonoBehaviour {
+	//cached children
+	public GameObject firstBodySegment;
+
+	//input
+	Vector3 inputVector;
+
+	//private members
+	float speed = 2f;
+	int length = 10;
+
+	void Start() {
+		firstBodySegment.GetComponent<PlayerBodySegment>().parent = this.gameObject;
+		firstBodySegment.GetComponent<PlayerBodySegment>().Duplicate(length, speed, this.gameObject);
+	}
+
+	void Update() {
+		HandleInput();
+		HandleMovement();
+	}
+
+	void HandleInput() {
+		inputVector.x = Input.GetAxis("Horizontal");
+		inputVector.y = Input.GetAxis("Vertical");
+	}
+
+	void HandleMovement() {
+		transform.position += inputVector.normalized * speed * Time.deltaTime;
+	}
+}
